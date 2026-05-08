f45dfdad53842b1f7863d3a2d29852f8f360ad2e
Sena <sena200753@gmail.com>
2026-05-08 19:41:10 +0300

    Add files via upload

---
+++ ödev.py
@@ -0,0 +1,16 @@
+ad = input("Adinizi girin: ")
+soyad = input("Soyadinizi girin: ")
+yas = int(input("Yasinizi girin: "))
+sehir = input("Yasadiğiniz şehir: ")
+meslek = input("Mesleğiniz: ")
+
+bes_yil_sonra_yas = yas + 5
+harf_sayisi = len(ad + soyad)   
+
+print(f"Ad Soyad : {ad} {soyad}")
+print(f"Yaş      : {yas}")
+print(f"Şehir    : {sehir}")
+print(f"Meslek   : {meslek}")
+
+print(f"5 yil sonraki yasiniz: {bes_yil_sonra_yas}")
+print(f"Ad ve soyad toplam harf sayisi: {harf_sayisi}") 
